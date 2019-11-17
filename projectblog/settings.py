3ad5b3a15371256bbfbf578fbea56c66fe83ecc8
FishWithMilk <mikegorobets@gmail.com>
2019-11-17 07:13:24 +0200

    staticchange

--- projectblog/settings.py
+++ projectblog/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
