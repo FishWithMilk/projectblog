a267bea6d1823574acf0f5f29707bb0eef431a41
FishWithMilk <mikegorobets@gmail.com>
2019-11-18 02:03:36 +0200

    Procfileandsettings

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn projectblog.wsgi
+web: python projectblog/manage.py collectstatic --noinput; bin/gunicorn_django --workers=4 --bind=0.0.0.0:$PORT projectblog/settings.py
--- projectblog/settings.py
+++ projectblog/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
